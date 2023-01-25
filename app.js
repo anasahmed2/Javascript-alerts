c568db76c8e58e8ecec9cd5190c51ccaa3813bc4
Anas Ahmed <123072865+anasahmed2@users.noreply.github.com>
2023-01-25 15:20:55 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+// ***** chapter 1
+
+// Question 1
+alert("Error! Please enter a valid password")
+
+
+// Question 2 
+alert("Welcome to JS Land...\nHappy Coding!")
+
+
+// Question3
+alert("Welcome to JS Land...")
+
+
+// Question 4
+alert("Happy Coding!\n Prevent this page from creating additional dailogs'")
+
+// Question 5
+alert("Hello... I can run JS through my web browser's console")
